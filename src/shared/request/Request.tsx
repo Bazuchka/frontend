887d4359df8ee1dab6bb450e163f6ce655e5f5d6
Соколов Константин Олегович <ksokolov@korusconsulting.ru>
2024-10-22 09:45:33 +0300

    22-10

--- src/shared/request/Request.tsx
+++ src/shared/request/Request.tsx
@@ -13,6 +13,7 @@ export type ResponseMessage = Record<string, string>;
 interface IAlertParams {
     errorMessage: string;
     throttleByMessage: boolean;
+    preventAlert: boolean;
 }
 
 const getAlertParams = (
@@ -21,25 +22,30 @@ const getAlertParams = (
 ): IAlertParams => {
     const hasErrorMessage = response && response.data && response.data.errorMessage;
     const isServerError = response === undefined || response.status >= 500;
-    const isClientError = response && response.status >= 400 && !options.preventDefaultAlert;
+    const isClientError = response && response.status >= 400;
 
     let errorMessage = hasErrorMessage
         ? t(`Shared:AlertMessages.${response.data.errorMessage}`)
         : "";
 
+    let preventAlert = false;
+
     if (isServerError) {
         errorMessage = hasErrorMessage ? errorMessage : t("Shared:AlertMessages.notResponsive");
     } else if (isClientError) {
         errorMessage = hasErrorMessage ? errorMessage : t("Shared:AlertMessages.wrongParams");
+        preventAlert = options.preventDefaultAlert ?? false;
     }
 
     // todo лучше избавиться от костыля и перейти к получение конкретного errorMessage с BE
     if (response?.status == 409) {
         errorMessage = t("Shared:AlertMessages.itemWithSameIdAlreadyExist");
+        preventAlert = options.preventDefaultAlert ?? false;
     }
 
     return {
         errorMessage,
+        preventAlert,
         throttleByMessage: isServerError,
     };
 };
@@ -50,13 +56,14 @@ export const request: Request = async (options: Options) => {
         return response;
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch ({ response }: any) {
-        const { errorMessage, throttleByMessage } = getAlertParams(response, options);
+        const { errorMessage, throttleByMessage, preventAlert } = getAlertParams(response, options);
 
-        viewStore.addAlert({
-            alertMode: "error",
-            message: errorMessage,
-            throttleByMessage,
-        });
+        !preventAlert &&
+            viewStore.addAlert({
+                alertMode: "error",
+                message: errorMessage,
+                throttleByMessage,
+            });
 
         return Promise.reject(response);
     }
