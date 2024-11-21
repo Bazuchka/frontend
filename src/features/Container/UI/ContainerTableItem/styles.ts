333b5ec20eb48857997ca7c3ae9de079248613a1
RZhekoldin <RZhekoldin@alabuga.ru>
2024-11-21 08:57:02 +0300

    Поправил верстку таблиц с кнопкой скачивания

--- src/features/Container/UI/ContainerTableItem/styles.ts
+++ src/features/Container/UI/ContainerTableItem/styles.ts
@@ -3,6 +3,7 @@ import { createUseStyles } from "react-jss";
 export const useStyles = createUseStyles(() => ({
     container: {
         position: "relative",
+        height: "100%",
     },
     button: {
         position: "absolute",
